cc7c3840c62668354be0739202acfc79f6467435
Stefano Mammola <68010284+StefanoMammola@users.noreply.github.com>
2022-10-19 07:35:40 +0200

    updated database

---
+++ Scripts/03.GetWoSdata.R
@@ -0,0 +1,108 @@
+##################
+# Species-level research effort
+# Created by Ricardo Correia
+# Updated 01/11/2021
+# Extracting references from Web of Science
+##################
+
+# Load relevant libraries
+library(wosr)
+library(readxl)
+library(tm)
+library(tidyverse)
+library(lubridate)
+
+# Open list of species
+sp_list <- as.data.frame(read_xlsx("./Data/SampleTREE_171022.xlsx"))
+head(sp_list)
+
+
+# Open research areas
+ra_list <- as.data.frame(read_xlsx("./Data/wos_research_areas.xlsx"))
+head(ra_list)
+
+
+# Selected research areas
+sel <- readRDS("./Data/sel_rareas.rds")
+sel <- ra_list[sel,]
+
+# Data frame to save selected search results
+wos_results_sel <- matrix(nrow = nrow(sp_list),
+                      ncol = nrow(sel)+2)
+wos_results_sel <- as.data.frame(wos_results_sel)
+colnames(wos_results_sel)[1:2] <- c("Species", "Total")
+colnames(wos_results_sel)[3:ncol(wos_results_sel)] <- sel[,1]
+wos_results_sel[,1] <- sp_list[,6]
+head(wos_results_sel)
+
+#sampling function
+extract.WoS <- function (q, s) {
+  
+  tryCatch(
+    
+    expr = {
+      
+      # extracting Wos records
+      q_sp <- query_wos(query = q,
+                        sid = s,
+                        editions = c("SCI", "SSCI", "AHCI", "ISTP", "ISSHP", "ESCI", "BSCI", "BHCI")) 
+      
+    },
+    
+    #if search fails, recursively call the same function...
+    error = function(e){
+      
+      print(" ... Internet search error occurred ... ")
+      Sys.sleep(30) #pause for 30 seconds
+      sid <- auth(username = NULL, password = NULL)
+      print(" ... Trying again ... ")
+      
+      #extracting WoS records
+      q_sp <- extract.WoS(q = q, s = s)
+      
+    }
+  )
+  
+  return(q_sp)
+}
+
+
+# Session authentication
+sid <- auth(username = NULL, password = NULL)
+# Sample WoS
+for (i in 1:nrow(sp_list)){
+  
+  if(i %in% seq(1,3200, 100)){
+    sid <- auth(username = NULL, password = NULL)
+  }
+  
+  # Set general species query
+  sp_query <- paste0("(TS=(\"", sp_list[i,6], "\"))")
+  q_sp <- extract.WoS(q = sp_query, s = sid)
+  
+  # Save results of general species query
+  wos_results_sel[i,2] <- q_sp$rec_cnt
+  
+  if(q_sp$rec_cnt > 0){
+    for (j in 1:nrow(sel)){
+      sp_query_ra <- paste0("(TS=(\"", sp_list[i,6], "\") AND SU=(\"", sel[j,1],"\"))")
+      q_sp_ra <- extract.WoS(q = sp_query_ra, s = sid)
+      
+      # Save results of general species query
+      wos_results_sel[i,j+2] <- q_sp_ra$rec_cnt
+      
+    }  
+  } else {
+    wos_results_sel[i,3:ncol(wos_results_sel)] <- 0
+  }
+  
+  print(paste0("Finished searches for species ", i, " out of ", nrow(sp_list)))
+  Sys.sleep(1)
+  
+}
+
+head(wos_results_sel)
+saveRDS(wos_results_sel, "./Data/wos_results_sel_20221017.rds")
+write.table(wos_results_sel, "./Data/wos_results_sel_20221017.txt", sep="\t", dec=".", row.names = F)
+
+
--- Scripts/05.Analyses.R
+++ Scripts/05.Analyses.R
@@ -21,7 +21,6 @@ library("tidyverse")
 # Loading the database  ---------------------------------------------------
 
 db <- read.csv2(file = "./Data/SampleTREE_171022.csv", sep = '\t', dec = ',', header = TRUE, as.is = FALSE)
-
 db <- db[db$FOSSIL != "1",]
 
 # Converting factors to factors
@@ -37,29 +36,110 @@ db$color_red        <- as.factor(db$color_red)
 # Data exploration --------------------------------------------------------
 ####################################################################################
 
-# Variable Y -------------------------------------------------------
+# Variable distribution & Collinearity ----------------------------------------------
 
-# Correlations
+##########################
+## Continuous variables ##
+##########################
 
+## Variables Y ##
+
+# Summary stats
+range(db$Total_wos,na.rm = TRUE) ; median(db$Total_wos,na.rm = TRUE)
+
+range(db$total_wiki_pgviews, na.rm = TRUE) ; median(db$total_wiki_pgviews, na.rm = TRUE)
+range(db$wiki_langs, na.rm = TRUE) ; median(db$wiki_langs, na.rm = TRUE)
+range(db$wiki_mean_month_pgviews, na.rm = TRUE) ; median(db$wiki_mean_month_pgviews, na.rm = TRUE)
+
+# Distribution
+ggplot(db, aes(x = Total_wos)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+ggplot(db, aes(x = Total_wos)) + geom_histogram(bins = 100) + theme_bw()
+
+ggplot(db, aes(x = total_wiki_pgviews)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+ggplot(db, aes(x = wiki_langs)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+ggplot(db, aes(x = wiki_mean_month_pgviews)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+# wiki_mean_month_pgviews has the best distribution
+
+# Correlations 
 db %>% dplyr::select(Total_wos, total_wiki_pgviews, wiki_langs, wiki_mean_month_pgviews) %>% 
   GGally::ggpairs() + theme_classic()
 
-# Y (distribution)
-ggplot(db, aes(x = total_wiki_pgviews)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
-ggplot(db, aes(x = total_wiki_pgviews)) + geom_histogram(bins = 100) + theme_bw()
-ggplot(db, aes(x = log(total_wiki_pgviews+1))) + geom_histogram() + theme_bw()
+## Variables X ##
 
-ggplot(db, aes(x = Total_wos)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
-ggplot(db, aes(x = Total_wos)) + geom_histogram(bins = 100) + theme_bw()
-ggplot(db, aes(x = log(Total_wos+1))) + geom_histogram() + theme_bw()
-
-# 
+# Distribution 
 ggplot(db, aes(x = uniqueness_family)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
 ggplot(db, aes(x = uniqueness_genus)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
 ggplot(db, aes(x = range_size)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+ggplot(db, aes(x = size_avg)) + geom_dotplot(binaxis='x', stackdir='center',dotsize=0.1) + theme_bw()
+
+plot(db$size_avg)
+#identify(db$size_avg)
+
+db[1980,] #change to 2.25 m??
+
+# Correlations 
+db %>% dplyr::select(uniqueness_family, uniqueness_genus, range_size, size_avg) %>% 
+  GGally::ggpairs() + theme_classic()
+
+#########################
+## Factorial variables ##
+#########################
+
+## Taxonomy ##
+table(db$kingdom)
+table(db$phylum)
+
+nlevels(db$kingdom)
+nlevels(db$phylum)
+nlevels(db$class)
+nlevels(db$order) #Devilish!
+nlevels(db$family)
+
+## Variables X ##
+
+table(db$model_organism) #not really usable
+table(db$harmful_to_human) #so so
+table(db$human_use) #OK!
+table(db$common_name) #OK!
+table(db$colorful) #OK!
+table(db$color_blu) #OK!
+table(db$color_red) #OK!
+
+table(db$IUCN) #OK!
 
 
-plot(log(db$range_size+1),db$Total_wos)
+
+ggplot(db, aes(x = phylum, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = phylum, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
+
+ggplot(db, aes(x = harmful_to_human, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = human_use, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = model_organism, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = common_name, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = colorful, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = color_blu, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = color_red, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = IUCN, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+
+
+
+
+# Data transformation -----------------------------------------------------
+
+# Balancing levels IUCN
+db$IUCN_rec <- db$IUCN
+
+levels(db$IUCN_rec) <- c("Endangered", "Unknown", "Endangered", "Endangered", "Least concern","Unknown","Least concern","Endangered")
+table(db$IUCN_rec)
+
+
+
+# Homogenize distirbution
+db <- db %>% 
+  dplyr::mutate(log_uniqueness_family = log(uniqueness_family+1),
+                log_uniqueness_genus = log(uniqueness_genus+1),
+                log_range_size = log(range_size+1),
+                log_size_avg = log(size_avg+1))
 
 # scaling size
 scale_size <- c()
@@ -74,6 +154,9 @@ plot(db$mean_divergence_time_Mya,db$Total_wos)
 
 plot(db$centroid_lat,db$total_wiki_pgviews)
 
+
+
+
 # Relationships
 
 ggplot(db, aes(x = phylum, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
@@ -86,6 +169,8 @@ ggplot(db, aes(x = common_name, y = log(Total_wos+1))) + geom_boxplot() + theme_
 ggplot(db, aes(x = colorful, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
 ggplot(db, aes(x = color_blu, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
 ggplot(db, aes(x = color_red, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = IUCN, y = log(Total_wos+1))) + geom_boxplot() + theme_bw() + coord_flip()
+
 
 ggplot(db, aes(x = harmful_to_human, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
 ggplot(db, aes(x = human_use, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
@@ -94,8 +179,12 @@ ggplot(db, aes(x = common_name, y = log(total_wiki_pgviews+1))) + geom_boxplot()
 ggplot(db, aes(x = colorful, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
 ggplot(db, aes(x = color_blu, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
 ggplot(db, aes(x = color_red, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
+ggplot(db, aes(x = IUCN, y = log(total_wiki_pgviews+1))) + geom_boxplot() + theme_bw() + coord_flip()
 
 db %>% dplyr::select(size_avg, mean_divergence_time_Mya, n_occurrences, range_size, range_size_AOO.y, range_size_MCP.y) %>% 
   GGally::ggpairs() + theme_classic()
+
+
+# Regression analyses -----------------------------------------------------
 
 
